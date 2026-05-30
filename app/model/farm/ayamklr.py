04d0bcec5fd10e1a97cb6ec26f1735d56c665e2b
limberbrevton0-ship-it <limberbrevton0@gmail.com>
2026-05-30 10:15:00 +0700

    report produksi revisi

--- app/model/farm/ayamklr.py
+++ app/model/farm/ayamklr.py
@@ -1,12 +1,10 @@
 import datetime
 
 from sqlmodel import SQLModel, Field
-from sqlalchemy import Column, Integer
-
 class Ayamklr(SQLModel, table=True):
     __tablename__ = "ayamklr"
 
-    tgl: datetime.date | None = Field(default=None)
+    Tgl: datetime.date | None = Field(default=None)
     Bulan: datetime.date | None = Field(default=None)
     ID: int = Field(default=None, primary_key=True, )
     mati: int | None = Field(default=None)
