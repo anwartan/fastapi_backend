b9b32b9b77f63387ad02322d12f9a39785dc4d5c
Walden <yukehandsome@gmail.com>
2026-07-26 21:27:33 +0700

    perubahan bborder

--- app/api/v1/kafe/bborder.py
+++ app/api/v1/kafe/bborder.py
@@ -178,7 +178,7 @@ def get(
     session: SessionDBKafe,
     session_login: SessionDBKafeLogin,
     Category: str,
-    limit: int = 10,
+    limit: int = 30,
     offset: int = 0,
     current_user: dict = Depends(get_current_user),
 ):
@@ -255,7 +255,7 @@ def get_bborder(
     session_login: SessionDBKafeLogin,
     category: str,
     divisi: str,
-    limit: int = 10,
+    limit: int = 30,
     offset: int = 0,
     current_user: dict = Depends(get_current_user),
 ):
